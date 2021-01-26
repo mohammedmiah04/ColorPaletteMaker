89c758deeba9373a967e445d6d2ee588d88472fd
MohammedM <nanumiah@nanus-mbp.home>
2021-01-26 17:56:43 +0000

    added clipboard func

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-used api [values.js](https://github.com/noeldelgado/values.js)
+added copy to clipboard function used api
+[values.js](https://github.com/noeldelgado/values.js)
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,15 +2,27 @@ import React, { useState, useEffect } from "react";
 import rgbToHex from "./utils";
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
-  const [alet, setAlert] = useState(false);
+  const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 1500);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
-      style={{ backgroundColor: `rgb(${bcg})` }}>
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}>
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
