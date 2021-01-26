ad87e4aa163fb6c1365569618cb900d74a0880d4
MohammedM <nanumiah@nanus-mbp.home>
2021-01-26 17:57:23 +0000

    Updates

---
+++ precache-manifest.4b11c480d21e72ef79fe689123076c2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6febd82b747b3f369326212572e21783",
+    "url": "/ColorPaletteMaker/index.html"
+  },
+  {
+    "revision": "24fa9fe5ed2253156e29",
+    "url": "/ColorPaletteMaker/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "82ad0eed29f5116b91af",
+    "url": "/ColorPaletteMaker/static/js/2.faa493fa.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/ColorPaletteMaker/static/js/2.faa493fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fa9fe5ed2253156e29",
+    "url": "/ColorPaletteMaker/static/js/main.eb34c49a.chunk.js"
+  },
+  {
+    "revision": "581c8ac21c0dd3087de7",
+    "url": "/ColorPaletteMaker/static/js/runtime-main.1c81ed6c.js"
+  }
+]);
